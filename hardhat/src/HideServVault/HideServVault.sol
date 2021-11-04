b8d4d40ab4eceac2a8c60cde846279cf45b70147
root <root@packer-output-e6bfc35f-2506-4e06-b5a0-4d92f15b8b17>
2021-11-04 10:08:45 +0000

    erc1155

--- hardhat/src/HideServVault/HideServVault.sol
+++ hardhat/src/HideServVault/HideServVault.sol
@@ -6,11 +6,14 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+import { ERC1155Receiver } from "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
+import { IERC1155 } from "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/IERC1155.sol";
+
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract HideServVault is Ownable, IERC721Receiver {
+contract HideServVault is Ownable, IERC721Receiver, ERC1155Receiver  {
         
     constructor () {
     }
@@ -23,11 +26,13 @@ contract HideServVault is Ownable, IERC721Receiver {
     struct TreasureDetails {
         string contractStandard;
         IERC721 ERC721contract;
+        IERC1155 ERC1155contract;
+        uint256 value;
         address source;
         uint256 tokenId;
     }
     
-    event NewTreasure(address nftContract, uint256 tokenId, string contractStandard);
+    event NewTreasure(address nftContract, uint256 tokenId, string contractStandard, uint256 value);
 
 
     function onERC721Received(address, address from, uint256 tokenId, bytes calldata) public virtual override returns (bytes4) {
@@ -38,14 +43,36 @@ contract HideServVault is Ownable, IERC721Receiver {
      
        treasureDetails[treasureId] = TreasureDetails({
             contractStandard: "ERC721",
-            ERC721contract: IERC721(msg.sender),
+            ERC721contract: IERC721(0x0000000000000000000000000000000000000000),
+            ERC1155contract: IERC1155(msg.sender),
+            value: 1,
             source: from,
             tokenId: tokenId
         });
-        emit NewTreasure(msg.sender, treasureId, "ERC721");
+        emit NewTreasure(msg.sender, treasureId, "ERC721", 1);
        _tokenIdCounter.increment();
         return this.onERC721Received.selector;
     }
-
     
+      function onERC1155Received(address operator, address from, uint256 tokenId, uint256 value, bytes calldata data) external override returns(bytes4) {
+            uint256 treasureId = _tokenIdCounter.current();
+            address tokenOwner = IERC721(msg.sender).ownerOf(tokenId);
+            
+          treasureDetails[treasureId] = TreasureDetails({
+            contractStandard: "ERC1155",
+            ERC721contract: IERC721(0x0000000000000000000000000000000000000000),
+            ERC1155contract: IERC1155(msg.sender),
+            value: value,
+            source: from,
+            tokenId: tokenId
+        });
+        emit NewTreasure(msg.sender, treasureId, "ERC1155", value);
+       _tokenIdCounter.increment();
+
+            return this.onERC1155BatchReceived.selector;
+        }
+
+    function onERC1155BatchReceived(address operator, address from, uint256[] calldata ids, uint256[] calldata values, bytes calldata data) external override returns(bytes4) {
+            return this.onERC1155BatchReceived.selector;
+        }
 } 
